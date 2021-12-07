84847e661101d2c14f9e92b5233fcd6ee5e00c93
Linus Ljung <linus.ljung@outlook.com>
2021-12-07 09:30:59 +0100

    Created tool for generating base template for new days

---
+++ tools/generateDay.js
@@ -0,0 +1,26 @@
+const fs = require('fs-extra');
+const path = require('path');
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+
+const TEMPLATE_FOLDER = path.resolve(__dirname, '../template');
+
+const { day } = yargs(hideBin(process.argv)).argv;
+
+if (!day) {
+  throw new Error('Missing `--day` argument');
+}
+
+const folderPath = path.resolve(__dirname, '../', day.toString());
+
+if (fs.existsSync(folderPath)) {
+  throw new Error(`Folder with path ${folderPath} already exists`);
+}
+
+fs.copy(TEMPLATE_FOLDER, folderPath, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Day ${day} generated`);
+});

1d5b4f333419a2b047255c9e805771e22334832b
Linus Ljung <linus.ljung@outlook.com>
2021-12-11 20:08:07 +0100

    [Day 11, Part 2] Find the first step where all octopuses are synchronized

--- 11/src/index.ts
+++ 11/src/index.ts
@@ -1,7 +1,8 @@
+import { clearAll } from './broadcaster';
 import getInput from './getInput';
 import getOctopuses from './getOctopuses';
 
-const octopuses = getOctopuses(getInput());
+let octopuses = getOctopuses(getInput());
 const steps = 100;
 
 for (let i = 0; i < steps; i++) {
@@ -15,3 +16,17 @@ console.log(
   `Number of flashes after ${steps} steps: `,
   octopuses.reduce((acc, curr) => acc + curr.flashes, 0)
 );
+
+clearAll();
+
+octopuses = getOctopuses(getInput());
+
+let i = 0;
+for (; octopuses.some((octopus) => !octopus.hasFlashed); i++) {
+  octopuses.forEach((octopus) => octopus.preStep());
+  octopuses.forEach((octopus) => octopus.step());
+  octopuses.forEach((octopus) => octopus.handleEnergyIncrease());
+  octopuses.forEach((octopus) => octopus.postStep());
+}
+
+console.log(`First synchronized step: ${i}`);
